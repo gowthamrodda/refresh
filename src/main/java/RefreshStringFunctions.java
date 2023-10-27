c319dd0f2ffc83d06c7f072e33a50b82c6d693ad
R Gowtham Kumar <gowthamkumar.r@carelon.com>
2023-10-27 07:55:54 +0530

    string functions

---
+++ src/main/java/RefreshStringFunctions.java
@@ -0,0 +1,19 @@
+public class RefreshStringFunctions {
+
+    String txt = "Hello World";
+
+    public static void resources() {
+        String name = "Bharat";
+
+        // print to uppercase
+        System.out.println("toUpperCase is " + name.toUpperCase());
+        // print to lowercase
+        System.out.println("toLowerCase is " + name.toLowerCase());
+
+        // concat example
+        System.out.println(name.concat(" is India"));
+
+        // Java Math class can be used for math operations
+        System.out.println(Math.max(5, 10));
+    }
+}
